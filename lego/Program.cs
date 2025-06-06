9b47829dcca4b4bb8231aec01ceae8bfe1321b4f
MaNnU <mannuspamm@gmail.com>
2025-06-06 13:34:15 +0200

    lkò

--- lego/Program.cs
+++ lego/Program.cs
@@ -1,4 +1,5 @@
-﻿using lego.model;
+﻿using lego.Logic;
+using lego.model;
 
 namespace lego
 {
@@ -7,11 +8,30 @@ namespace lego
         static void Main(string[] args)
         {
             var context = new LegoContext();
-            var parts = context.LegoParts.ToList();
-            foreach (var part in parts)
+            var colorRepo = new LegoRepository<LegoColor>(context);
+            var colors = colorRepo.GetAll();
+            foreach (var color in colors)
             {
-                Console.WriteLine($"Part ID: {part.PartNum}, Name: {part.Name}");
+                Console.WriteLine($"Part ID: {color.Id}, Name: {color.Name}");
             }
+
+
+            var inventoryRepo = new LegoRepository<LegoInventory>(context);
+            var inventories = inventoryRepo.GetAll();
+            foreach (var inventory in inventories)
+            {
+                Console.WriteLine($"Inventory ID: {inventory.Id}, Set ID: {inventory.Version}");
+            }
+
+            
+
+
+            //    var parts = context.LegoParts.ToList();
+            //    foreach (var part in parts)
+            //    {
+            //        Console.WriteLine($"Part ID: {part.PartNum}, Name: {part.Name}");
+            //    }
+            //}
         }
     }
 }
